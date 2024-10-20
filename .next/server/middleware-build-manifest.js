1c1c5e35255e4f8aa7eb7a2921a57b6f0c70fbdf
[aleexnager] <[norbacpull@gmail.com]>
2024-10-20 13:10:09 +0200

    add animation to navbar

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-f5d767265a0c1bf8.js","static/chunks/fd9d1056-36eb487bea0e3d91.js","static/chunks/117-02cde7f60f8662f8.js","static/chunks/main-app-f400729bef19f579.js"],pages:{"/_app":["static/chunks/webpack-f5d767265a0c1bf8.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-e5f2ad12ff08d704.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-f5d767265a0c1bf8.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-e5f2ad12ff08d704.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import NavLink from "./NavLink";
+import { motion } from "framer-motion";
 
 const MenuOverlay = ({ links, setNavbarOpen, isExternalPage }) => {
   return (
-    <ul className="flex flex-col py-4 items-center">
-      {links.map((link, index) => (
-        <li key={index}>
-          <NavLink
-            id={link.id}
-            href={link.path}
-            title={link.title}
-            setNavbarOpen={setNavbarOpen}
-            isExternalPage={isExternalPage}
-          />
-        </li>
-      ))}
-    </ul>
+    <motion.div
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: "auto", opacity: 1 }}
+      exit={{ height: 0, opacity: 0 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      className="overflow-hidden bg-[#121212] text-white"
+    >
+      <ul className="flex flex-col py-4 items-center">
+        {links.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              id={link.id}
+              href={link.path}
+              title={link.title}
+              setNavbarOpen={setNavbarOpen}
+              isExternalPage={isExternalPage}
+            />
+          </li>
+        ))}
+      </ul>
+    </motion.div>
   );
 };
 
